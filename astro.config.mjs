f1b368dfbc3024d70aef6950e52976b9b2228112
Maria Elisa Gammaldi <megammaldi@gmail.com>
2024-09-17 10:11:41 +0200

    Fix github pages upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: `https://megammaldi.github.io`,
+  base: "portfolio",
+  integrations: [tailwind()],
 });
